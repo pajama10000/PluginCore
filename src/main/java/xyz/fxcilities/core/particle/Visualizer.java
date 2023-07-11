040c63e7864a147bdf04a607042e7ca03cd9c342
pajama <99984344+pajama10000@users.noreply.github.com>
2023-07-10 22:47:20 -0400

    Update Visualizer.java

--- src/main/java/xyz/fxcilities/core/particle/Visualizer.java
+++ src/main/java/xyz/fxcilities/core/particle/Visualizer.java
@@ -1,83 +1,47 @@
-package xyz.fxcilities.core.particle;
+package xyz.fxcilities.core.particle
 
-import org.bukkit.Location;
-import org.bukkit.World;
-import org.bukkit.plugin.Plugin;
-import org.bukkit.scheduler.BukkitRunnable;
-import org.bukkit.util.BoundingBox;
+import org.bukkit.Location
+import org.bukkit.World
+import org.bukkit.plugin.Plugin
+import org.bukkit.scheduler.BukkitRunnable
+import org.bukkit.util.BoundingBox
 
-/**
- * @author Fxcilities
- * @author Mrmagicpie Visualize a BoundingBox with particles
- *     <p>Example:
- *     <pre>{@code
- * ParticleBuilder particle = new ParticleBuilder(Particle.REDSTONE, new Particle.DustOptions(Color.PURPLE, 1)); // Purple redstone particle
- *
- * World world = Bukkit.getWorld("myWorld");
- * Location corner1 = new Location(world, 10, 10, 10);
- * Location corner2 = new Location(world, 20, 20, 20);
- * BoundingBox box = BoundingBox.of(corner1, corner2);
- *
- * Visualizer visualizer = new Visualizer(boundingBox, particleBuilder, world);
- * visualizer.runTaskTimer(Core.getInstance(), 1, 0); // Run constantly
- * // You can stop it with visualizer.cancel();
- * }</pre>
- */
-public class Visualizer extends BukkitRunnable {
-    private final BoundingBox bb;
-    private final ParticleBuilder particleBuilder;
-    private final World world;
+class Visualizer(
+    private val bb: BoundingBox,
+    private val particleBuilder: ParticleBuilder,
+    private val world: World
+) : BukkitRunnable() {
 
-    /**
-     * Creates a Visualizer object
-     *
-     * @param bb Bounding box the visualizer will display
-     * @param particleBuilder The particle that will outline the bounding box
-     * @param world The world that the visualizer will display in
-     */
-    public Visualizer(BoundingBox bb, ParticleBuilder particleBuilder, World world) {
-        this.bb = bb;
-        this.world = world;
-        this.particleBuilder = particleBuilder;
-    }
+    override fun run() {
+        val coords = arrayOf(
+            doubleArrayOf(bb.minX, bb.minY, bb.minZ) to doubleArrayOf(bb.maxX, bb.minY, bb.maxZ),
+            doubleArrayOf(bb.maxX, bb.maxY, bb.maxZ) to doubleArrayOf(bb.minX, bb.maxY, bb.minZ),
+            doubleArrayOf(bb.maxX, bb.maxY, bb.minZ) to doubleArrayOf(bb.minX, bb.minY, bb.minZ),
+            doubleArrayOf(bb.minX, bb.maxY, bb.minZ) to doubleArrayOf(bb.minX, bb.minY, bb.maxZ),
+            doubleArrayOf(bb.minX, bb.maxY, bb.maxZ) to doubleArrayOf(bb.maxX, bb.minY, bb.maxZ),
+            doubleArrayOf(bb.maxX, bb.maxY, bb.minZ) to doubleArrayOf(bb.maxX, bb.minY, bb.maxZ)
+        )
 
-    /**
-     * @see BukkitRunnable#runTaskTimer(Plugin, long, long)
-     */
-    @Override
-    public void run() {
-        for (double[][] coords :
-                new double[][][] {
-                        // Below are all the coordinates for the following wireframe calls.
-                        // First Location Coords                          Second Location Coords
-                        { { bb.getMinX(), bb.getMinY(), bb.getMinZ() },  { bb.getMaxX(), bb.getMinY(), bb.getMaxZ() } },
-                        { { bb.getMaxX(), bb.getMaxY(), bb.getMaxZ() },  { bb.getMinX(), bb.getMaxY(), bb.getMinZ() } },
-                        { { bb.getMaxX(), bb.getMaxY(), bb.getMinZ() },  { bb.getMinX(), bb.getMinY(), bb.getMinZ() } },
-                        { { bb.getMinX(), bb.getMaxY(), bb.getMinZ() },  { bb.getMinX(), bb.getMinY(), bb.getMaxZ() } },
-                        { { bb.getMinX(), bb.getMaxY(), bb.getMaxZ() },  { bb.getMaxX(), bb.getMinY(), bb.getMaxZ() } },
-                        { { bb.getMaxX(), bb.getMaxY(), bb.getMinZ() },  { bb.getMaxX(), bb.getMinY(), bb.getMaxZ() } }
-                }
-        ) {
-            double[] locOne = coords[0]; // I don't like this but it's more efficient - Pie
-            double[] locTwo = coords[1];
-            wireframe(new BoundingBox(locOne[0], locOne[1], locOne[2], locTwo[0], locTwo[1], locTwo[2]));
+        for (coordsPair in coords) {
+            val locOne = coordsPair.first
+            val locTwo = coordsPair.second
+            val box = BoundingBox(
+                locOne[0], locOne[1], locOne[2],
+                locTwo[0], locTwo[1], locTwo[2]
+            )
+            wireframe(box)
         }
     }
 
-    /**
-     * Simulates a wireframe between two corners with particles.
-     *
-     * @param box the bounding box to be used for the wireframe
-     */
-    private void wireframe(BoundingBox box) {
-        for (double x = box.getMinX(); x <= box.getMaxX(); x++)
-            for (double y = box.getMinY(); y <= box.getMaxY(); y++)
-                for (double z = box.getMinZ(); z <= box.getMaxZ(); z++) {
-
-                    Location loc = new Location(world, x, y, z);
+    private fun wireframe(box: BoundingBox) {
+        for (x in box.minX..box.maxX)
+            for (y in box.minY..box.maxY)
+                for (z in box.minZ..box.maxZ) {
+                    val loc = Location(world, x, y, z)
                     if (particleBuilder.shouldUseData())
-                        world.spawnParticle(particleBuilder.particle, loc, 0, particleBuilder.data);
-                    else world.spawnParticle(particleBuilder.particle, loc, 0);
+                        world.spawnParticle(particleBuilder.particle, loc, 0, particleBuilder.data)
+                    else
+                        world.spawnParticle(particleBuilder.particle, loc, 0)
                 }
     }
 }
