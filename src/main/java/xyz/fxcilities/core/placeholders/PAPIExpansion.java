f970850e804785be74b8f43e064096a5d2c97eef
pajama <99984344+pajama10000@users.noreply.github.com>
2023-07-10 22:44:49 -0400

    Update PAPIExpansion.java

--- src/main/java/xyz/fxcilities/core/placeholders/PAPIExpansion.java
+++ src/main/java/xyz/fxcilities/core/placeholders/PAPIExpansion.java
@@ -1,79 +1,33 @@
-package xyz.fxcilities.core.placeholders;
+package xyz.fxcilities.core.placeholders
 
-import me.clip.placeholderapi.expansion.PlaceholderExpansion;
+import me.clip.placeholderapi.expansion.PlaceholderExpansion
+import org.bukkit.entity.Player
+import xyz.fxcilities.core.Core
+import xyz.fxcilities.core.placeholders.handlers.ExtensionHandler
 
-import org.bukkit.entity.Player;
+class PAPIExpansion(private val manager: ExtensionHandler) : PlaceholderExpansion() {
 
-import xyz.fxcilities.core.Core;
-import xyz.fxcilities.core.placeholders.handlers.ExtensionHandler;
-
-import java.util.ArrayList;
-import java.util.List;
-
-/**
- * Manages a PlaceholderAPI extension Example:
- *
- * <pre>{@code
- * new PAPIExpansion(new ExtensionHandler() {
- *
- *     @Override
- *     public String getPrefix() {
- *         return "example";
- *     }
- *
- *     @Override
- *     public String onRequest(Player player, String placeholder) {
- *         // Registers two placeholders (%example_uuid%, and %example_name%)
- *         String result = null;
- *         switch (placeholder) {
- *             case "uuid":
- *                 result = player.getUniqueId().toString();
- *                 break;
- *             case "name":
- *                 result = player.getName();
- *                 break;
- *         }
- *         return result;
- *     }
- * });
- * }</pre>
- *
- * @see PlaceholderExpansion
- */
-public class PAPIExpansion extends PlaceholderExpansion {
-
-    public static List<PAPIExpansion> expansions = new ArrayList<>();
-
-    private final ExtensionHandler manager;
-
-    /**
-     * Register and initialize PAPI extension
-     *
-     * @param manager The extension handler. Can be created with a lambda or {@code new
-     *     ExtensionHandler() { ... }}
-     */
-    public PAPIExpansion(ExtensionHandler manager) {
-        this.manager = manager;
-        expansions.add(this);
+    companion object {
+        val expansions: MutableList<PAPIExpansion> = mutableListOf()
     }
 
-    @Override
-    public String getIdentifier() {
-        return manager.getPrefix();
+    init {
+        expansions.add(this)
     }
 
-    @Override
-    public String getAuthor() {
-        return String.join(", ", Core.getInstance().getDescription().getAuthors());
+    override fun getIdentifier(): String {
+        return manager.getPrefix()
     }
 
-    @Override
-    public String getVersion() {
-        return Core.getInstance().getPluginVersion();
+    override fun getAuthor(): String {
+        return Core.getInstance().description.authors.joinToString(", ")
     }
 
-    @Override
-    public String onPlaceholderRequest(Player player, String params) {
-        return manager.onRequest(player, params);
+    override fun getVersion(): String {
+        return Core.getInstance().pluginVersion
+    }
+
+    override fun onPlaceholderRequest(player: Player, params: String): String {
+        return manager.onRequest(player, params)
     }
 }
