cc53893e1017e20e267dd57c533a06cf581624d2
pajama <99984344+pajama10000@users.noreply.github.com>
2023-07-10 22:46:31 -0400

    Update ParticleBuilder.java

--- src/main/java/xyz/fxcilities/core/particle/ParticleBuilder.java
+++ src/main/java/xyz/fxcilities/core/particle/ParticleBuilder.java
@@ -1,35 +1,10 @@
-package xyz.fxcilities.core.particle;
+package xyz.fxcilities.core.particle
 
-import org.bukkit.Particle;
+import org.bukkit.Particle
 
-import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
+class ParticleBuilder(val particle: Particle, val data: Any? = null) {
 
-/** Represents a particle with particle data includes */
-public class ParticleBuilder {
-    public Particle particle;
-    public Object data;
-
-    /**
-     * @param particle The particle
-     * @param data The particle data
-     */
-    public ParticleBuilder(@Nonnull Particle particle, @Nullable Object data) {
-        this.particle = particle;
-        this.data = data;
-    }
-
-    /**
-     * @param particle The particle
-     */
-    public ParticleBuilder(@Nonnull Particle particle) {
-        this(particle, null);
-    }
-
-    /**
-     * @return if the particle needs to use its custom data
-     */
-    public boolean shouldUseData() {
-        return data == null || particle.getDataType() != Void.class;
+    fun shouldUseData(): Boolean {
+        return data == null || particle.dataType != Void::class.java
     }
 }
