02a47a35bbacf08d50b9c49f5f0df3b8ec91731c
pajama <99984344+pajama10000@users.noreply.github.com>
2023-07-10 23:14:20 -0400

    Update and rename Global.java to Global.kt

--- src/main/java/xyz/fxcilities/core/Global.java
+++
@@ -1,6 +0,0 @@
-package xyz.fxcilities.core;
-
-public interface Global {
-    String CONSOLE_LINE = "______________________________________________________________";
-    String CHAT_LINE = "&m-----------------------------------------------------";
-}
---
+++ src/main/java/xyz/fxcilities/core/Global.kt
@@ -0,0 +1,8 @@
+package xyz.fxcilities.core
+
+interface Global {
+    companion object {
+        const val CONSOLE_LINE = "______________________________________________________________"
+        const val CHAT_LINE = "&m-----------------------------------------------------"
+    }
+}
