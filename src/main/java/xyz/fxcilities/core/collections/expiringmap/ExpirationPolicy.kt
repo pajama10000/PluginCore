fbbadcd5b1994f1e2abdbda5a39ee25f0d5d2642
pajama <99984344+pajama10000@users.noreply.github.com>
2023-07-10 23:46:44 -0400

    Update and rename ExpirationPolicy.java to ExpirationPolicy.kt

--- src/main/java/xyz/fxcilities/core/collections/expiringmap/ExpirationPolicy.kt
+++ src/main/java/xyz/fxcilities/core/collections/expiringmap/ExpirationPolicy.kt
@@ -1,9 +1,9 @@
-package xyz.fxcilities.core.collections.expiringmap;
+package xyz.fxcilities.core.collections.expiringmap
 
 /** Determines how ExpiringMap entries should be expired. */
-public enum ExpirationPolicy {
+enum class ExpirationPolicy {
     /** Expires entries based on when they were last accessed */
     ACCESSED,
     /** Expires entries based on when they were created */
-    CREATED;
+    CREATED
 }
